e83deea3e9163f7cb35599461431e5c488aa09c1
Sajaawadallah <sajaawdalla@gmail.com>
2023-07-02 06:38:10 +0300

    end sliders and cards

--- js/main.js
+++ js/main.js
@@ -3,80 +3,82 @@ new WOW().init();
 
 
 
-// slider for clints opinin 
-
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 20,
-//     speed: 2000,
-//     navigation: {
-//         nextEl: ".next1",
-//         prevEl: ".prev1",
-//     },
-//     loop: true,
-
-//     breakpoints: {
-
-//         773: {
-//             slidesPerView: 2,
-//             spaceBetween: 40,
-//         },
-//         1200: {
-//             slidesPerView: 3,
-//             spaceBetween: 60,
-//         },
-//     },
+// slider for our partners  
 
 
-// });
+new Swiper(".mySwiper1", {
+    autoplay: true,
+    speed: 2000,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 0,
 
-// swiper.el.addEventListener("mouseenter", function () {
-//     swiper.autoplay.stop();
-// });
+    },
+    loop: true,
+    breakpoints: {
 
-// swiper.el.addEventListener("mouseleave", function () {
-//     swiper.autoplay.start();
-// });
 
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        575: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
 
 
 
-// // slider for our clints  
+});
 
 
-// new Swiper(".mySwiper1", {
-//     autoplay: true,
-//     navigation: {
-//         nextEl: ".next",
-//         prevEl: ".prev",
-//     },
-//     speed: 2000,
-//     slidesPerView: 2,
-//     spaceBetween: 20,
-//     autoplay: {
-//         delay: 0,
-
-//     },
-//     loop: true,
-//     breakpoints: {
-
-//         575: {
-//             slidesPerView: 3,
-//             spaceBetween: 30,
-//         },
-//         992: {
-//             slidesPerView: 4,
-//             spaceBetween: 40,
-//         },
-//         1200: {
-//             slidesPerView: 5,
-//             spaceBetween: 50,
-//         },
-//     },
+// slider for our clints  
 
 
+new Swiper(".mySwiper", {
+    autoplay: true,
+    speed: 2000,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 0,
 
-// });
+    },
+    loop: true,
+    breakpoints: {
+
+
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        575: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+        1200: {
+            slidesPerView: 6,
+            spaceBetween: 60,
+        },
+    },
+
+
+
+});
 
 
 // slider for our expe 
