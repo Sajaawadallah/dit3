5dc24ec908dc6df6a9e692828c971f015e33ec3f
Sajaawadallah <sajaawdalla@gmail.com>
2023-07-02 21:11:40 +0300

    end all page

--- js/main.js
+++ js/main.js
@@ -105,3 +105,28 @@ new Swiper(".mySwiper2", {
         },
     },
 });
+
+
+
+
+// form validtion 
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation');
+
+
+    Array.from(forms)
+        .forEach(function (form) {
+
+            form.addEventListener('submit', function (event) {
+
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
